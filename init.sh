7aae04a12b55f9e7a28e82caccea0dc791b07e74
Erik Sutherland <esutherland@live.com>
2015-01-20 02:03:44 -0800

    Antigen Zshing

--- init.sh
+++ init.sh
@@ -4,20 +4,21 @@
 dotFileDir="$HOME/dotfiles"
 
 # Load the following dotfiles
-dotFiles=("vimrc")
+dotFiles=("vimrc" "zshrc")
 
 for i in "${dotFiles[@]}"; do
-    # Ensure that the file exists in the source directory
-    if [ -e "$dotFileDir/$i" ]; then
-        echo "File $i exists already"
-        continue
-    fi
+   # Ensure that the file exists in the source directory
+   if [ -e "$dotFileDir/$i" ]; then
+      # Ensure that the file doesn't already exist in the destination dir
+      if [ -e "$HOME/.$i" ]; then
+         echo "File $i exists already"
+      else
+         ln -s $dotFileDir/$i $HOME/.$i
+      fi
+   else
+      echo "Source file $i does not exist"
+      continue
+   fi
 
-    # Ensure that the file doesn't already exist in the destination dir
-    if [ -e "~/.$i" ]; then
-        echo "File $i exists already"
-    else
-        ln -s $dotFileDir/$i $HOME/.$i
-    fi
 done
 
---
+++ zshrc
@@ -0,0 +1,23 @@
+source $HOME/dotfiles/zsh/antigen/antigen.zsh
+
+# Load the framework
+antigen use oh-my-zsh
+
+# Got to get the git
+antigen bundle git
+
+# Hehehe
+antigen bundle nyan
+
+# Some web stuff I do
+antigen bundle node
+antigen bundle npm
+
+# That awesome syntax highlighting
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Get my favorite theme
+antigen theme bureau
+
+# Apply all the above declarations
+antigen apply
