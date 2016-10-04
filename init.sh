0abec7fbdbd0df30babd396cf39824adf125ba91
Erik Sutherland <erik.misu@gmail.com>
2016-10-03 22:17:24 -0700

    redeem init.sh

--- init.sh
+++ init.sh
@@ -1,56 +1,22 @@
 #!/bin/bash
 
-# default expectation
-DOT_FILE_DIR="$HOME/dotfiles"
+# initialize all submodules everywhere
+git submodule --init --recursive
 
-# linkerate :: String -> String -> IO (LinkFromAToB)
-linkerate() {
-    if [ ! "$1" ]; then
-        echo "LINKERATE: Missing Source param"
-        return -1
-    fi
-    if [ ! "$2" ]; then
-        echo "LINKERATE: Missing Destination Param"
-        return -1
-    fi
-    [ ! -e "$HOME/$2" ]       || (echo "LINKERATE: $2 Exists"; return -1) && \
-    [ -e "$DOT_FILE_DIR/$1" ] || (echo "LINKERATE: Source file does not exist"; return -1) && \
-    ln -s "$DOT_FILE_DIR/$1" "$HOME/$2"
-}
+# Where is this repo? Presuppose you run this script from the dotfiles repo
+DOT_FILE_DIR=$PWD
 
-linkerate aliases       .aliases
-linkerate ctags         .ctags
-linkerate ghci          .ghci
-linkerate gitconfig     .gitconfig
-linkerate npmrc         .npmrc
-linkerate pentadactylrc .pentadactylrc
-linkerate profile       .profile
-linkerate tmux.conf     .tmux.conf
-linkerate vimrc         .vimrc
-linkerate vimrc         .ideavimrc
+# Install dot files
+ln -s $DOT_FILE_DIR/ghci $HOME/.ghci
+ln -s $DOT_FILE_DIR/git-commit-template $HOME/.git-commit-template
+ln -s $DOT_FILE_DIR/screenrc $HOME/.screenrc
+ln -s $DOT_FILE_DIR/vimperatorrc $HOME/.vimperatorrc
 
-# mkdir -p ~/.vim/autoload
-# cd ~/.vim/autoload
-# https://github.com/tpope/vim-pathogen.git
+# Vim install
+ln -s $DOT_FILE_DIR/.vimrc $HOME/.vimrc
+ln -s $DOT_FILE_DIR/.vim $HOME/.vim
 
-# mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-# curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+ln -s $DOT_FILE_DIR/.zshrc $HOME/.zshrc
 
-# Vim source
-# ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
-
-# YouCompleteMe in vim requires that you install some packages and build the
-# module
-# git submodule --recursive update $dotFileDir
-
-# Necessary packages
-# PKG_INSTALL=("build-essential" "python-dev" "cmake")
-
-# Updata and install necessary stuff
-# sudo apt-get update
-# sudo apt-get install $PKG_INSTALL
-
-
-# cd vim/bundle/vimproc.vim
-# make
-
+# Install NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
