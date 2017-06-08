723f86620d229dc2f77b5857c6746c101cab55cf
Erik Sutherland <erik.misu@gmail.com>
2017-06-08 12:49:38 -0700

    tune init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Where is this repo? Presuppose you run this script from the dotfiles repo
+# Install the toys
+sudo apt update
+sudo apt install rofi xmonad git slock vim screen zsh
+
+# Assume the dotfils repo is in $HOME
 DOT_FILE_DIR=$HOME/dotfiles
 
 cd $DOT_FILE_DIR
@@ -8,12 +12,17 @@ cd $DOT_FILE_DIR
 # initialize all submodules everywhere
 git submodule update --init --recursive
 
+# initialize powerline fonts
+./fonts/install.sh
+
+# copy git config, because of the jorb
 cp $DOT_FILE_DIR/gitconfig $HOME/.gitconfig
 
+# link all atom configs
 mkdir -p $DOT_FILE_DIR/.atom
 ln -s $DOT_FILE_DIR/atom/* $HOME/.atom/
 
-
+# link all aliases
 ln -s $DOT_FILE_DIR/alias $HOME/.alias
 
 # Install dot files
@@ -21,7 +30,6 @@ ln -s $DOT_FILE_DIR/ghci $HOME/.ghci
 ln -s $DOT_FILE_DIR/haskeline $HOME/.haskeline
 ln -s $DOT_FILE_DIR/xmonad $HOME/.xmonad
 ln -s $DOT_FILE_DIR/xmobarrc $HOME/.xmobarrc
-
 ln -s $DOT_FILE_DIR/git-commit-template $HOME/.git-commit-template
 ln -s $DOT_FILE_DIR/screenrc $HOME/.screenrc
 
